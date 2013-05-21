f7d8476d1c47dd688b9c26e0c9fc197c3df46316
Harry Garrood <hdgarrood@gmail.com>
2013-05-21 21:47:16 +0100

    breaks everything. initial attempt at adding websockets

--- haskell/Main.hs
+++ haskell/Main.hs
@@ -11,22 +11,47 @@ import Web.Scotty                           (scotty,
 import Network.Wai                          (Middleware)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
 import Data.Aeson                           (encode)
+import qualified Data.Text as T
+import qualified Data.ByteString.Lazy as B
+import qualified Network.WebSockets as WS
 
 import World
 import SafeStaticDataFileMiddleware         (safeStaticDataFiles)
 
+-- for now, a client is just a sink
+type Client = WS.Sink WS.Rfc6455
+type ServerState = (World, [Client])
+
+newServerState :: ServerState
+newServerState = (makeWorld, [])
+
+broadcast :: ByteString -> ServerState -> IO ()
+broadcast message clients = do
+    T.putStrLn $ "broadcasting: " ++ message
+    mapM_ (\s -> WS.sendSink s $ WS.binaryData message) clients
+
+webSocketServerPort :: Int
+webSocketServerPort = 9160
+
+startWebSocketsThread :: MVar ServerState -> IO ()
+startWebSocketsThread state = do
+    WS.runServer "0.0.0.0" webSocketServerPort $ application state
+
+application :: MVar ServerState -> WS.Request -> WS.WebSockets WS.Rfc6455 ()
+application state rq = do
+    WS.acceptRequest rq
+    T.putStrLn "New connection"
+    forever $ do
+
 main :: IO ()
-main = scotty 3000 $ do
-    middleware logStdoutDev
-    middleware safeStaticDataFiles
+main = do
+    state <- newMVar newServerState
 
-    world <- liftIO $ makeWorld >>= newMVar
+    forkIO $ startWebSocketsThread state
 
-    get "/" $ do
-        redirect "/static/index.html"
+    scotty 3000 $ do
+        middleware logStdoutDev
+        middleware safeStaticDataFiles
 
-    get "/iterate" $ do
-        wl <- liftIO $ takeMVar world
-        let wl' = iterateWorld wl
-        liftIO $ putMVar world wl'
-        json wl'
+        get "/" $ do
+            redirect "/static/index.html"
