9ecdc108b4f636273772147146f627d76035ed67
Harry Garrood <hdgarrood@gmail.com>
2013-06-01 19:59:04 +0100

    add mime types

--- src/haskell/FileEmbedMiddleware.hs
+++ src/haskell/FileEmbedMiddleware.hs
@@ -4,6 +4,7 @@ module FileEmbedMiddleware (fileEmbed) where
 
 import qualified Data.ByteString as B
 import qualified Data.Text as T
+import qualified Data.Map as M
 import Data.Text.Encoding (encodeUtf8)
 import Data.Conduit (ResourceT)
 import System.FilePath
@@ -18,23 +19,43 @@ fileEmbed embeddedFiles app req =
         Just x  -> respond x
         Nothing -> app req
 
-respond :: B.ByteString -> ResourceT IO Response
-respond bs =
+respond :: (B.ByteString, B.ByteString) -> ResourceT IO Response
+respond (contents, mimeType) =
     return $
         ResponseBuilder
             status200
-            [("Content-Type", "text/plain")] 
-            (fromByteString bs)
+            [("Content-Type", mimeType)] 
+            (fromByteString contents)
 
+-- Given a list of embedded files, and a request's pathinfo, return a
+-- (contents, mimeType) pair, or Nothing
 getEmbeddedFile ::
     [(FilePath, B.ByteString)] ->
     [T.Text] ->
-    Maybe B.ByteString
-getEmbeddedFile embeddedFiles pathInfo =
-    getEmbedPath pathInfo >>= (\p -> lookup p embeddedFiles)
+    Maybe (B.ByteString, B.ByteString)
+getEmbeddedFile embeddedFiles pathInfo = do
+    contents <- getEmbedPath pathInfo >>= (\p -> lookup p embeddedFiles)
+    let mimeType = getMime $ T.unpack $ last pathInfo
+    return (contents, mimeType)
 
 getEmbedPath :: [T.Text] -> Maybe String
 getEmbedPath pathInfo =
     case pathInfo of
         "static":xs -> Just $ T.unpack $ T.intercalate "/" xs
         _           -> Nothing
+
+getMime :: FilePath -> B.ByteString
+getMime fp =
+    let (_, ext) = splitExtension fp
+    in  M.findWithDefault defaultMimeType fp mimeMap
+
+defaultMimeType :: B.ByteString
+defaultMimeType = "application/octet-stream"
+    
+-- A mapping of file extensions to MIME types
+mimeMap :: M.Map String B.ByteString
+mimeMap = M.fromList $
+    [ (".html", "text/html")
+    , (".js", "text/javascript")
+    , (".css,", "text/css")
+    ]
