c24c4b9f159fa35ad6d5d468a42b63a0b8178b60
Harry Garrood <harry@garrood.me>
2013-09-17 11:39:05 -0400

    Add TokenGenerator (for player tokens)

---
+++ src/haskell/TokenGenerator.hs
@@ -0,0 +1,30 @@
+module TokenGenerator where
+
+import           System.Random
+import           Control.Monad
+import           Control.Monad.Random
+import qualified Data.ByteString as B
+import           Data.ByteString (ByteString)
+import           Data.ByteString.Char8 (singleton)
+
+data TokenGenerator = TokenGenerator
+    { randomGen :: StdGen
+    }
+
+-- arbitrarily chosen
+tokenLength :: Int
+tokenLength = 50
+
+nextToken :: TokenGenerator -> (ByteString, TokenGenerator)
+nextToken tokGen =
+    let (token, randomGen') = runRand generateToken $ randomGen tokGen
+    in  (token, tokGen { randomGen = randomGen' })
+
+generateToken :: Rand StdGen ByteString
+generateToken = do
+    chars <- replicateM tokenLength generateTokenChar
+    return $ foldl1 B.append chars
+
+generateTokenChar :: Rand StdGen ByteString
+generateTokenChar =
+    fmap singleton $ getRandomR ('a', 'z')
