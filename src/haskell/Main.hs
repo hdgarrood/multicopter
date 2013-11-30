e8374bbe8889c5fc62c1f2f3df714699f903cb3e
Harry Garrood <harry@garrood.me>
2013-11-30 09:44:33 -0500

    Refactor setting cookies

--- src/haskell/Main.hs
+++ src/haskell/Main.hs
@@ -4,22 +4,27 @@ import Control.Monad.IO.Class               (liftIO)
 import Control.Monad.Writer                 (runWriter)
 import Control.Monad.Reader
 
-import Web.Scotty.Trans
-
-import Network.Wai                          (Middleware)
-import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Data.Aeson                           (encode)
-import Data.Text.Lazy                       (Text)
-import qualified Data.Text.Lazy as T
-import Data.Text.Lazy.Encoding               (decodeUtf8)
-import qualified Data.ByteString as BS hiding (putStrLn)
-import qualified Data.ByteString.Char8 as BS (putStrLn)
-import qualified Data.ByteString.Lazy as BSL hiding (putStrLn)
-import qualified Data.ByteString.Lazy.Char8 as BSL (putStrLn)
-import qualified Network.WebSockets as WS
+import           Network.Wai                    (Middleware)
+import qualified Network.WebSockets             as WS
 import qualified Network.WebSockets.Util.PubSub as WS
-import Data.FileEmbed                       (embedDir)
-import qualified Data.Text.Format as TF
+import           Web.Scotty.Trans
+import           Web.Cookie
+
+import Data.Aeson (encode)
+
+import           Data.Text.Lazy          (Text)
+import qualified Data.Text.Lazy          as T
+import qualified Data.Text.Lazy.Builder  as T
+import qualified Data.Text.Lazy.Encoding as T
+import qualified Data.Text.Format        as TF
+
+import qualified Data.ByteString            as BS hiding  (putStrLn)
+import qualified Data.ByteString.Char8      as BS         (putStrLn)
+import qualified Data.ByteString.Lazy       as BSL hiding (putStrLn)
+import qualified Data.ByteString.Lazy.Char8 as BSL        (putStrLn)
+import qualified Blaze.ByteString.Builder   as Builder
+
+import Data.FileEmbed (embedDir)
 
 import World
 import FileEmbedMiddleware                  (fileEmbed)
@@ -30,6 +35,58 @@ import Player
 
 strictToLazy :: BS.ByteString -> BSL.ByteString
 strictToLazy = BSL.fromChunks . (: [])
+
+lazyToStrict :: BSL.ByteString -> BS.ByteString
+lazyToStrict = BS.concat . BSL.toChunks
+
+makeCookie :: Text -> Text -> SetCookie
+makeCookie n v = def { setCookieName = n', setCookieValue = v' }
+    where
+        conv = lazyToStrict . T.encodeUtf8
+        n' = conv n
+        v' = conv v
+
+renderSetCookie' :: SetCookie -> Text
+renderSetCookie' = T.decodeUtf8 . Builder.toLazyByteString . renderSetCookie
+
+setCookie :: Text -> Text -> ActionT WebM ()
+setCookie n v = setHeader "Set-Cookie" (renderSetCookie' (makeCookie n v))
+
+startScottyThread :: TVar ServerState -> IO ()
+startScottyThread tvar = do
+    let runM m = runReaderT (runWebM m) tvar
+        runActionToIO = runM
+
+    scottyT 3000 runM runActionToIO $ do
+        -- middleware logStdoutDev
+        -- middleware safeStaticDataFiles
+        -- middleware $ fileEmbed $(embedDir "src/static")
+
+        get "/" $ do
+            redirect "/register"
+
+        get "/register" $ do
+            render registrationForm
+
+        post "/register" $ do
+            (do name <- param "name"
+                player <- webM $ modifyWith (\pr -> addPlayer pr name)
+                setCookie "auth_token"
+                    (T.decodeUtf8 $ strictToLazy $ token player)
+                redirect "/register")
+            `rescue` (\_ -> redirect "/register")
+
+        get "/registered-players" $ do
+            players <- webM $ gets getAllPlayers
+            render (registeredPlayers players)
+
+main :: IO ()
+main = do
+    tvar <- newServerState >>= newTVarIO
+
+    -- void $ forkIO $ startGameThread       mstate
+    -- void $ forkIO $ startWebSocketsThread mstate
+    startScottyThread     tvar
 
 -- webSocketServerPort :: Int
 -- webSocketServerPort = 9160
@@ -61,49 +118,3 @@ strictToLazy = BSL.fromChunks . (: [])
 --         let message     = encode changes
 --         B.putStrLn $ "sending " `B.append` message
 --         WS.publish pubSub $ WS.textData $ decodeUtf8 message
-
--- Maximum cookie age, in seconds
-maxCookieAge :: Int
-maxCookieAge = 86400
-
-makeCookieString :: Text -> Text -> Text
-makeCookieString k v = TF.format "{}={}; Expires={}" (k, v, maxCookieAge)
-
-setCookie :: Text -> Text -> ActionT WebM ()
-setCookie k v = setHeader "Set-Cookie" (makeCookieString k v)
-
-startScottyThread :: TVar ServerState -> IO ()
-startScottyThread tvar = do
-    let runM m = runReaderT (runWebM m) tvar
-        runActionToIO = runM
-
-    scottyT 3000 runM runActionToIO $ do
-        middleware logStdoutDev
-        -- middleware safeStaticDataFiles
-        -- middleware $ fileEmbed $(embedDir "src/static")
-
-        get "/" $ do
-            redirect "/register"
-
-        get "/register" $ do
-            render registrationForm
-
-        post "/register" $ do
-            (do name <- param "name"
-                player <- webM $ modifyWith (\pr -> addPlayer pr name)
-                setCookie "auth_token"
-                    (decodeUtf8 $ strictToLazy $ token player)
-                redirect "/register")
-            `rescue` (\_ -> redirect "/register")
-
-        get "/registered-players" $ do
-            players <- webM $ gets getAllPlayers
-            render (registeredPlayers players)
-
-main :: IO ()
-main = do
-    tvar <- newServerState >>= newTVarIO
-
-    -- void $ forkIO $ startGameThread       mstate
-    -- void $ forkIO $ startWebSocketsThread mstate
-    startScottyThread     tvar
