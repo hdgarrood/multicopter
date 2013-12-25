176bd628fd45fc195666e503c76ebaedf9e1953d
Harry Garrood <hdgarrood@gmail.com>
2013-12-25 13:14:17 +0000

    switch to wai-app-static

--- src/haskell/Server/StaticFileMiddleware.hs
+++ src/haskell/Server/StaticFileMiddleware.hs
@@ -1,10 +1,18 @@
 module Server.StaticFileMiddleware where
 
-import Network.Wai
-import Network.Wai.Middleware.Static
+import qualified Network.Wai as W
+import Network.Wai.Application.Static
 
-staticFiles :: Middleware
-staticFiles = staticPolicy multicopterPolicy
+staticFiles :: W.Middleware
+staticFiles app req =
+    maybe (app req)
+          staticFilesApp
+          (modifyReq req)
 
-multicopterPolicy :: Policy
-multicopterPolicy = noDots >-> hasPrefix "static" >-> addBase "src"
+modifyReq :: W.Request -> Maybe W.Request
+modifyReq req = case W.pathInfo req of
+    "static":xs -> Just $ req { W.pathInfo = xs }
+    _           -> Nothing
+
+staticFilesApp :: W.Application
+staticFilesApp = staticApp $ defaultFileServerSettings "src/static"
